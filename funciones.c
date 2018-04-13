52c76de82e3f88d096548ba9407d75fd7ee14f51
acevedo92 <36174619+acevedo92@users.noreply.github.com>
2018-04-13 18:41:26 -0300

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -1,32 +1,32 @@
-int suma (int a,int b)
+int suma (int a,int b) //recibe el operando a y el operando b y los suma
 {
     int res;
     res=a+b;
     return res;
 }
 
-int resta(int a,int b)
+int resta(int a,int b) //recibe el operando a y el operando b y los resta
 {
     float res;
     res=a-b;
     return res;
 }
 
-float division(float a, float b)
+float division(float a, float b) //recibe el operando a y el operando b y los divide
 {
     float res;
     res=a/b;
     return res;
 }
 
-int producto(int a,int b)
+int producto(int a,int b) //recibe el operando a y el operando b y los multiplica
 {
     int res;
     res=a*b;
     return res;
 }
 
-long long int factorial(int a)
+long long int factorial(int a) //recibe el operando a y devuelve el factorial
 {
     long long int res=1;
     int i;
